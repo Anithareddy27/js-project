d4278e3b2c1ce0fe0100a5865d73b43dea89b5b0
Anithareddy27 <banithareddy1@gmail.com>
2025-11-20 22:19:54 +0530

    third commit

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ sidebar.style.display = "none";
 }, 300);
 });
 
-async function loadCategories() {
+/*async function loadCategories() {
 let res = await fetch("https://www.themealdb.com/api/json/v1/1/categories.php");
 let data = await res.json();
 showCategoryList(data.categories);
@@ -35,4 +35,34 @@ function showCategoryCards(categories) {
             <h3>${item.strCategory}</h3>
         </div>
     `).join("");
+}*/
+const apiUrl = "https://www.themealdb.com/api/json/v1/1/categories.php";
+
+async function fetchCategories() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        displayCategories(data.categories);
+    } catch (error) {
+        console.log("Error:", error);
+    }
 }
+
+function displayCategories(categories) {
+    const container = document.getElementById("categoryContainer");
+
+    const cards = categories.map(cat => {
+        return `
+            <div class="card">
+            <div class="tag">${cat.strCategory}</div>
+            <img src="${cat.strCategoryThumb}" alt="${cat.strCategory}">
+               
+            </div>
+        `;
+    });
+
+    container.innerHTML = cards.join("");
+}
+
+// Call function
+fetchCategories();
